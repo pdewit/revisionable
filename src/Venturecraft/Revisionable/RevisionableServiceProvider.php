3708c92863701f3fede8bed4366efc6218b1f09d
Patrick de Wit <patrick1193@gmail.com>
2016-11-11 22:30:15 +0100

    Add service provider for migrations

---
+++ src/Venturecraft/Revisionable/RevisionableServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Venturecraft\Revisionable;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class RevisionableServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+    }
+
+}
