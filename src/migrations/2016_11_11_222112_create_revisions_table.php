e97dcba7b00cd25def835cde15ca8e7844688347
Patrick de Wit <patrick1193@gmail.com>
2016-11-11 22:22:53 +0100

    Add impersonation id to revision

---
+++ src/migrations/2016_11_11_222112_create_revisions_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRevisionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('revisions', function ($table) {
+            $table->integer('impersonate_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('revisions');
+    }
+}
