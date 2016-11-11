d65e2c35a1c5dfe0ea4a0f72f7489e06accf12cb
Patrick de Wit <patrick1193@gmail.com>
2016-11-12 00:07:41 +0100

    Apply impersonation id to trait as well

--- src/Venturecraft/Revisionable/RevisionableTrait.php
+++ src/Venturecraft/Revisionable/RevisionableTrait.php
@@ -182,6 +182,7 @@ trait RevisionableTrait
                     'old_value' => array_get($this->originalData, $key),
                     'new_value' => $this->updatedData[$key],
                     'user_id' => $this->getSystemUserId(),
+                    'impersonate_id' => session('impersonate', null),
                     'created_at' => new \DateTime(),
                     'updated_at' => new \DateTime(),
                 );
@@ -224,6 +225,7 @@ trait RevisionableTrait
                 'old_value' => null,
                 'new_value' => $this->{self::CREATED_AT},
                 'user_id' => $this->getSystemUserId(),
+                'impersonate_id' => session('impersonate', null),
                 'created_at' => new \DateTime(),
                 'updated_at' => new \DateTime(),
             );
@@ -251,6 +253,7 @@ trait RevisionableTrait
                 'old_value' => null,
                 'new_value' => $this->{$this->getDeletedAtColumn()},
                 'user_id' => $this->getSystemUserId(),
+                'impersonate_id' => session('impersonate', null),
                 'created_at' => new \DateTime(),
                 'updated_at' => new \DateTime(),
             );
